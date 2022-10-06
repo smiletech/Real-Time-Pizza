47d54247d38ccd63a613561c3df3d44b22b8d664
Navin kumar <navinsmileraj@gmail.com>
2022-10-06 15:51:23 +0530

    first

---
+++ server.js
@@ -0,0 +1,22 @@
+const express=require("express")
+const ejs=require("ejs")
+const path=require("path")
+const expressLayouts=require("express-ejs-layouts")
+const app=express();
+
+app.get("/",(req,res)=>{res.render('home')});
+
+
+
+// set templet engine
+app.use(expressLayouts);
+app.set('views',path.join(__dirname,'resources/views'));
+app.set('view engine','ejs');
+
+
+
+const PORT =process.env.PORT||3000;
+
+app.listen(PORT,()=>{
+    console.log(`listing Port on ${PORT}`)
+});
